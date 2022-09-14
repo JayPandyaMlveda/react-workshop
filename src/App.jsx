cc52fcfe915184a1b4b7a250e37d4d6f07a49127
Jay Pandya <jay.pandya@mlveda.com>
2022-09-14 19:49:46 +0530

    added lorem

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hiiii Hello</h1>
-      <h2> ty </h2>
+      
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id tempora deleniti a quas labore dolore ut, et, rerum iure quidem fuga tenetur illo magni! Ipsa ducimus atque ratione a cumque!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
