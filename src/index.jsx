5b5ecb6183bac9d9c80b900b0d0e2818c6f04aaa
Jay Pandya <jay.pandya@mlveda.com>
2022-09-16 16:51:25 +0530

    Color change

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
+import Greetings from "./Grettings";
+
+const tableStyle = {
+  background: "beige",
+  textAlign: "center",
+  width: "50%",
+  margin: "0 auto",
+  border: "solid",
+  borderColor:"black"
+};
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <table className="t1" style={tableStyle}>
+      <tbody>
+      <tr>
+        <th>Name</th>
+        <th>Birthday</th>
+        <th>Mobile no</th>
+      </tr>
+      <Greetings name="J" bday="1/2" mobileNo="123456" />
+      <Greetings name="D" bday="1/2" mobileNo="12345678" />
+      <Greetings  name="P" bday="1/2" mobileNo="123456789" />
+      </tbody>
+    </table>
   </React.StrictMode>,
   document.getElementById("root")
 );
-
