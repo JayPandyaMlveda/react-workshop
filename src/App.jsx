992f88796993c2381926ed61ebb61a4bfacf9fdd
Jay Pandya <jay.pandya@mlveda.com>
2022-09-21 07:28:18 +0530

    todoApp

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react'
+import Todo from './Components/Todo';
 function App() {
-  return (
-    <div className="App">
-      
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id tempora deleniti a quas labore dolore ut, et, rerum iure quidem fuga tenetur illo magni! Ipsa ducimus atque ratione a cumque!</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <Todo/>
+        </div>
+    )
 }
 
 export default App;
