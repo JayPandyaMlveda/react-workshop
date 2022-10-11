7be5b8b8ae07ca4d46b89eb4b051073b7adad868
Jay Pandya <jay.pandya@mlveda.com>
2022-10-11 19:21:03 +0530

    delete using axios

--- src/Components/ShowTodo.jsx
+++ src/Components/ShowTodo.jsx
@@ -1,17 +1,36 @@
 // edit enable then change h6 to input and change edit button text to cancle text and also show save button
 // once click on cancle button then change it to edit and change input to h6
 
+import axios from "axios";
 import { useState } from "react";
-
 
 function ShowTodo({ task, id, children, onEdit }) {
   const [editEnable, setEditEnable] = useState(false);
   const [inputText, setInputText] = useState("");
-
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  async function onSaveClick() {
+    setLoading(true);
+    setError(null);
+    try {
+      await axios.put(`http://localhost:3000/todos/${id}`, {
+        task: inputText,
+      });
+      onEdit(inputText);
+      setEditEnable(false);
+    } catch (error) {
+      setLoading(false);
+      setError(error);
+    }
+  }
+  if (error) {
+    throw error;
+  }
   return (
     <div className="d-flex flex-row gap-2 align-items-center justify-content-center">
       {editEnable && (
         <input
+          disabled={loading}
           value={inputText}
           style={{ flexGrow: 1 }}
           onChange={(e) => setInputText(e.target.value)}
@@ -21,6 +40,7 @@ function ShowTodo({ task, id, children, onEdit }) {
 
       {children}
       <button
+        disabled={loading}
         className="btn btn-light ml-4"
         onClick={() => {
           setEditEnable(!editEnable);
@@ -33,11 +53,9 @@ function ShowTodo({ task, id, children, onEdit }) {
       </button>
       {editEnable && (
         <button
+          disabled={loading}
           className="btn btn-primary"
-          onClick={() => {
-            onEdit(inputText);
-            setEditEnable(false);
-          }}
+          onClick={onSaveClick}
         >
           Save
         </button>
